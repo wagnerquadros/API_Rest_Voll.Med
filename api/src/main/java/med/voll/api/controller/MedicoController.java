53eb8f60988ad13fd934f8c836c09d8bd0eb99f1
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2023-12-31 13:41:00 -0300

    Migratins Flayway - Criando tabela medicos

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,7 +4,6 @@ import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
