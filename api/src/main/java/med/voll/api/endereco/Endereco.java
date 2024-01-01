4259345c4c4845ecd4ab8f284835336bebd2b410
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2024-01-01 09:14:08 -0300

    Atualizar dados Medico/Paciente

--- api/src/main/java/med/voll/api/endereco/Endereco.java
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -23,9 +23,33 @@ public class Endereco {
         this.logradouro = dados.logradouro();
         this.bairro = dados.bairro();
         this.cep = dados.cep();
-        this.uf = dados.uf();
-        this.cidade = dados.cidade();
         this.numero = dados.numero();
         this.complemento = dados.complemento();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+    }
+
+    public void atualizarEndereco(DadosEndereco dados) {
+
+        if(dados.logradouro() != null)
+            this.logradouro = dados.logradouro();
+
+        if(dados.bairro() != null)
+            this.bairro = dados.bairro();
+
+        if(dados.cep() != null)
+            this.cep = dados.cep();
+
+        if(dados.numero() != null)
+            this.numero = dados.numero();
+
+        if(dados.complemento() != null)
+            this.complemento = dados.complemento();
+
+        if(dados.cidade() != null)
+            this.cidade = dados.cidade();
+
+        if(dados.uf() != null)
+            this.uf = dados.uf();
     }
 }
