4e4886718631d62c4e8f305746b4a28a8bee5a70
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2024-01-01 10:53:39 -0300

    BD V5 - Pacientes ResponseEntity

--- api/src/main/java/med/voll/api/paciente/Paciente.java
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -28,7 +28,10 @@ public class Paciente {
     @Embedded
     private Endereco endereco;
 
+    private boolean ativo;
+
     public Paciente(DadosCadastroPaciente dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -47,5 +50,9 @@ public class Paciente {
         if (dados.endereco() != null)
             this.endereco.atualizarEndereco(dados.endereco());
 
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
