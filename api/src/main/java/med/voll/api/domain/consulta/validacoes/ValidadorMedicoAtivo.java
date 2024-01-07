cd705b4337bc24c8a920cabbf1dd13201a773e5b
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2024-01-07 09:34:00 -0300

    Validações

---
+++ api/src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.infra.exception.ValidacaoException;
+
+public class ValidadorMedicoAtivo {
+
+    private MedicoRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+        if(dados.idMedico() == null){
+            return;
+        }
+
+        var medicoEstaAtivo = repository.findAtivoById(dados.idMedico());
+        if(!medicoEstaAtivo){
+            throw new ValidacaoException("COnsulta não pode ser agendada com o médico exclido.");
+        }
+    }
+}
---
+++ api/src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorPacienteSemOutraConsultaNoDia.java
@@ -0,0 +1,15 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+public class ValidadorPacienteSemOutraConsultaNoDia {
+
+    private ConsultaRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+        var primeiroHorario = dados.data().withHour(7);
+        var ultimoHorario = dados.data().withHour(18);
+        var pacientePossuiOutraConsultaNoDia = repository.existsByPacienteIdAndDataBetween(dados.idPaciente(), dados.data());
+    }
+}
